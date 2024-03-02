428b3b03a9153dccdd548656092ac9d659c51454
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-03-03 00:09:52 +0530

    Sign in api routes done

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/authController.js';
+import { signup, signin} from '../controllers/authController.js';
 
 const router = express.Router();
 
-
+router.post('/signin', signin);
 router.post('/signup', signup);
 
 export default router;
