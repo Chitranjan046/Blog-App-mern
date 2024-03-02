1b004e50d4ff6bd781f88f3383b55369e044dec2
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-03-02 13:23:35 +0530

    create the header section, create and run the server and Database Connection don

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,21 +3,25 @@ import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
-import Products from './pages/Products';
 import SignUp from './pages/SignUp';
-
+import Header from './components/Header';
+import Research from "./pages/Research";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
 
 export default function App() {
   return (
     <BrowserRouter>
-    
+    <Header />
     <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/dashboard' element={<Dashboard/>} />
-        <Route path='/products' element={<Products/>} />
+        <Route path='/research' element={<Research/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/projects' element={<Projects/>} />
 
 
     </Routes>
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+app.use(cors());
+connectDB();
+app.use(express.json());
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
