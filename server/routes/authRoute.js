9eec8d57226af23d4cac4c65b51b79c6462b89c3
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-03-03 13:25:27 +0530

    Update Authentication

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -3,8 +3,9 @@ import { googleAuth, signup, signin} from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.post('/signin', signin);
 router.post('/signup', signup);
+router.post('/signin', signin);
 router.post('/google', googleAuth);
 
 export default router;
+
