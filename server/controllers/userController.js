68074cc5babc5027c451f85bbde085ee0e199a38
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-03-03 22:17:25 +0530

    Complete profile page functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,6 +6,9 @@ export const test = (req, res) => {
   res.json({ message: 'API is working!' });
 };
 
+
+
+
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to update this user'));
@@ -17,7 +20,7 @@ export const updateUser = async (req, res, next) => {
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
   if (req.body.username) {
-    if (req.body.username.length < 9 || req.body.username.length > 22) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
         errorHandler(400, 'Username must be between 7 and 20 characters')
       );
@@ -33,23 +36,23 @@ export const updateUser = async (req, res, next) => {
         errorHandler(400, 'Username can only contain letters and numbers')
       );
     }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
-          },
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
-        { new: true }
-      );
-      const { password, ...rest } = updatedUser._doc;
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
-    }
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
